3a8a98c31bf5e3ef05712132c0eb503569d4ceda
emillyalmeida <emillyalmeidan@gmail.com>
2022-09-13 22:14:53 -0300

    feat(router): create config to routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,38 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Home from "../views/Home/index.vue";
+import Credencials from "../views/Credentails";
+import Feedbacks from "../views/Feedback";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: Home,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/credentials",
+    name: "Credentials",
+    component: Credencials,
+    meta: {
+      hasAuth: true,
+    },
+  },
+  {
+    path: "/feedbacks",
+    name: "Feedbacks",
+    component: Feedbacks,
+    meta: {
+      hasAuth: true,
+    },
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: { name: "Home" },
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory("/"),
   routes,
 });
 
